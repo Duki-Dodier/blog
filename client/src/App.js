ff99549d73501928863a27f85fffb7e7fd9b5489
duki <duki.dodier@gmail.com>
2023-06-23 13:30:31 -0300

    estutura inicial do projeto REACT

---
+++ client/src/App.js
@@ -0,0 +1,76 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Register from "../src/pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+//components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Navbar />
+        <Home />
+        <Footer />
+      </div>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <div>
+        <Navbar />
+        <Login />
+        <Footer />
+      </div>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <div>
+        <Navbar />
+        <Register />
+        <Footer />
+      </div>
+    ),
+  },
+  {
+    path: "/post/:id",
+    element: (
+      <div>
+        <Navbar />
+        <Single />
+        <Footer />
+      </div>
+    ),
+  },
+  {
+    path: "/write",
+    element: (
+      <div>
+        <Navbar />
+        <Write />
+        <Footer />
+      </div>
+    ),
+  },
+]);
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
